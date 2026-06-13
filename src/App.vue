719dda687d9e64a7eaa8b3e07439d4f967d6e2c5
Nam Hung Truong <hungtn2711@gmail.com>
2026-06-13 16:26:51 +0700

    Vue + Boots nav. edit
    Setup Vue Router and Bootstrap navigation

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,27 @@
 <template>
-  <router-view />
-</template> 
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <router-link class="navbar-brand" to="/">
+        GameHub
+      </router-link>
+
+      <div class="navbar-nav">
+        <router-link class="nav-link" to="/">
+          Home
+        </router-link>
+
+        <router-link class="nav-link" to="/news">
+          News
+        </router-link>
+
+        <router-link class="nav-link" to="/about">
+          About
+        </router-link>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mt-4">
+    <router-view />
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>GameHub</h1>
+    <p>Welcome to GameHub!</p>
+  </div>
+</template>
